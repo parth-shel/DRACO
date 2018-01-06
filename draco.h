e036144f8af240343c8c84768568976005d175c8
parth-shel <ps@parthshel.com>
2018-01-06 03:40:16 -0500

    made some changes

--- draco.h
+++ draco.h
@@ -4,8 +4,24 @@
 // @version v:0.1 - Jan 6, 2018
 // @author parth_shel
 
-int * bitmap;
-bool * visited;
+#include<stdlib>
+#include<stdio>
+#include<vector>
+
+#define IMAGE_WIDTH 10 //COLUMNS
+#define IMAGE_HEIGHT 10 //ROWS
+#define DEFAULT_BKCOLR 0 //BGI BLACK
+
+int bitmap [IMAGE_WIDTH] [IMAGE_HEIGHT] = {DEFAULT_BKCOLOR};
+bool visited [IMAGE_WIDTH] [IMAGE_HEIGHT] = {false};
+
+struct Pixel {
+	int color;
+	int x;
+	int y;
+};
+
+std::vector<Pixel> border;
 
 void compress(char * inputFile, char * outputFile) {
 	return;
