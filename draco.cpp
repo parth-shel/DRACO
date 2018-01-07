9eac7316d201e19bdf8fbb337024275ac030d56c
parth-shel <ps@parthshel.com>
2018-01-07 13:50:26 -0500

    made some changes

--- draco.cpp
+++ draco.cpp
@@ -31,6 +31,7 @@ int main(int argc, char ** argv) {
 				exit(-1);
 			}
 			fclose(check);
+			printf("here2\n");
 			strcpy(outputFile, argv[2]);
 			strcat(outputFile, ".draco");
 			printf("here\n");
